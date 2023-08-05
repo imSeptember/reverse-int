a4eaada04a44ed19790aedd0e542d48b5f4b55f8
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-08-05 21:16:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = function reverse (n) {
-   let splited = n.split("");
+   let string = n.toString();
+   let splited = string.split("");
    let reversed = splited.reverse();
    let joined = reversed.join("");
-    return joined;
+    return parseFloat(joined);
 }
